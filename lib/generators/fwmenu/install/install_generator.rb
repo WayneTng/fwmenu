21f2fea38e1f43d098964ebc8dd742d8ee96305b
Brian <brian@futureworkz.com>
2015-10-27 16:38:53 +0700

    update install_generator.rb

--- lib/generators/fwmenu/install/install_generator.rb
+++ lib/generators/fwmenu/install/install_generator.rb
@@ -23,7 +23,7 @@ module Fwmenu
 
       def copy_initializer_file
         template "menu.rb", "app/models/#{file_name}.rb"
-        template "_get_menu_for.html.erb", "app/views/_get_#{file_name}_for.html.erb"
+        copy_file "_get_menu_for.html.erb", "app/views/_get_#{file_name}_for.html.erb"
         template "menu_helper.rb", "app/helpers/#{file_name}_helper.rb"
       end
     end
