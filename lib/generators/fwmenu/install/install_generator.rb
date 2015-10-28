ab602fc49daefc08710ab0f64413ba97d1e0abf3
Brian <brian@futureworkz.com>
2015-10-28 16:22:54 +0700

    update install_generator.rb

--- lib/generators/fwmenu/install/install_generator.rb
+++ lib/generators/fwmenu/install/install_generator.rb
@@ -1,4 +1,5 @@
 require 'rails/generators/migration'
+require 'bundler'
 
 module Fwmenu
   module Generators
@@ -27,6 +28,12 @@ module Fwmenu
         template "menu_item.rb", "app/models/#{file_name}_item.rb"
         copy_file "_get_menu_for.html.erb", "app/views/_get_#{file_name}_for.html.erb"
         template "menu_helper.rb", "app/helpers/#{file_name}_helper.rb"
+
+        active = Bundler.load.specs.map { |spec| spec.name }
+        if active.include? "activeadmin"
+          template "activeadmin/menu.rb", "app/admin/#{file_name}.rb"
+          template "activeadmin/menu_item.rb", "app/admin/#{file_name}_item.rb"
+        end
       end
     end
   end
