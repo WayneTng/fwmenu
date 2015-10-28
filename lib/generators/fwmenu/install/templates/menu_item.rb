2f22859772e5df0df45d07c6bc82932b00a98768
Brian <brian@futureworkz.com>
2015-10-28 16:05:01 +0700

    update menu_item model

--- lib/generators/fwmenu/install/templates/menu_item.rb
+++ lib/generators/fwmenu/install/templates/menu_item.rb
@@ -4,6 +4,9 @@ class <%= file_name.camelize %>Item < ActiveRecord::Base
 	validates :title, :<%= file_name %>, presence: true
 
 	belongs_to :<%= file_name %>
+	belongs_to :<%= file_name %>_item
 
-	enumerize :page, in: Rails.application.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.sort
+	enumerize :page, in: Rails.application.routes.routes.collect {|r| r.path.spec.to_s }.compact.uniq.delete_if{|i|i.include? "admin" or i.include? "rails" }.sort
+	# enumerize :page, in: Rails.app lication.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.delete_if{|i|i.include? "admin"}.sort
+
 end
