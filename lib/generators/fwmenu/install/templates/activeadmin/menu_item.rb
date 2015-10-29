1d3699dc4be96bfdbde34bf97352257c35c5f322
Brian <brian@futureworkz.com>
2015-10-29 15:35:22 +0700

    update menu_item for active admin

--- lib/generators/fwmenu/install/templates/activeadmin/menu_item.rb
+++ lib/generators/fwmenu/install/templates/activeadmin/menu_item.rb
@@ -19,7 +19,7 @@ ActiveAdmin.register <%= file_name.camelize %>Item do
       f.input :title
       f.input :link
       f.input :page, as: :select, collection: Rails.application.routes.routes.collect {|r| r.path.spec.to_s.gsub("(.:format)", "") }.compact.uniq.delete_if{|i|i.include? "admin" or i.include? "rails" }.sort
-      f.input :show
+      f.input :show, label: "Id (Only for other components)"
       f.input :article, label: "Articles (Only for article page)"
       f.input :internal_link
       f.input :<%= file_name %>_item, label: "Parent"
