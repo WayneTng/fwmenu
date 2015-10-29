91fce874b7ce5fd8ed40d48d0929d2383b01be80
Brian <brian@futureworkz.com>
2015-10-29 10:37:28 +0700

    update article model

--- lib/generators/fwmenu/install/templates/article.rb
+++ lib/generators/fwmenu/install/templates/article.rb
@@ -1,7 +1,16 @@
 class Article < ActiveRecord::Base
+	extend Enumerize
 	validates :title, :description, presence: true
 	
 	extend FriendlyId
   
   friendly_id :title, use: [:slugged, :finders]
+
+  before_save :set_slug
+
+  enumerize :layout, in: Dir.glob('app/views/articles/show/*').collect{|r| r.gsub("app/views/", "").split('.').first}
+
+  def set_slug
+  	self.slug = title.parameterize unless slug.present?
+  end
 end
