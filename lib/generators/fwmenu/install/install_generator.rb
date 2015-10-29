280e8814ad1ae2bf48be6c5e35d241bfe83ea787
Brian <brian@futureworkz.com>
2015-10-29 09:06:43 +0700

    update install_generator.rb

--- lib/generators/fwmenu/install/install_generator.rb
+++ lib/generators/fwmenu/install/install_generator.rb
@@ -20,7 +20,9 @@ module Fwmenu
       def copy_migrations
         migration_template "create_menus.rb", "db/migrate/create_#{file_name}s.rb"
         migration_template "create_menu_items.rb", "db/migrate/create_#{file_name}_items.rb"
+        migration_template "create_articles.rb", "db/migrate/create_articles.rb"
         migration_template "add_reference_menu_items_to_menu_item.rb", "db/migrate/add_reference_#{file_name}_items_to_#{file_name}_item.rb"
+        migration_template "add_reference_menu_items_to_article.rb", "db/migrate/add_reference_#{file_name}_items_to_article.rb"
       end
 
       def copy_initializer_file
@@ -29,11 +31,13 @@ module Fwmenu
         template "menu.rb", "app/models/#{file_name}.rb"
         copy_file "_get_menu_for.html.erb", "app/views/_get_#{file_name}_for.html.erb"
         template "menu_helper.rb", "app/helpers/#{file_name}_helper.rb"
+        template "article.rb", "app/models/article.rb"
         
         if active.include? "activeadmin"
           template "activeadmin/model/menu_item.rb", "app/models/#{file_name}_item.rb"
           template "activeadmin/menu.rb", "app/admin/#{file_name}.rb"
           template "activeadmin/menu_item.rb", "app/admin/#{file_name}_item.rb"
+          template "activeadmin/article.rb", "app/admin/article.rb"
         else
           template "menu_item.rb", "app/models/#{file_name}_item.rb"
         end
