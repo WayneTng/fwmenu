b7ddbf8e82ea6ba97cc6a711ca673e9fdbad931f
Brian <brian@futureworkz.com>
2015-10-29 15:35:44 +0700

    update menu_helper

--- lib/generators/fwmenu/install/templates/helpers/menu_helper.rb
+++ lib/generators/fwmenu/install/templates/helpers/menu_helper.rb
@@ -1,5 +1,20 @@
 module <%= class_name %>Helper
   def get_menus_for
-    <%= class_name %>.where(published: true)
+    <%= class_name %>.where(published: true).where("length(<%= table_name %>.position) > 0")
   end
+
+  def get_link(item)
+		if item.internal_link && item.page.present?
+			if item.page.to_s == "/articles/:id" && item.article.present?
+				link_to item.title, article_url(item.article)
+			elsif item.page.include? ":id"
+				link = item.page.gsub(":id", item.show.to_s)
+				link_to item.title, link
+			else
+				link_to item.title, item.page
+			end
+		else
+			link_to item.title, item.link, target: :_blank
+		end
+	end
 end
