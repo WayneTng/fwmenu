61f1caf2f173f8609715cade00de33aebf15d817
Brian <brian@futureworkz.com>
2015-10-29 09:06:17 +0700

    add article and update menu_item for activeadmin

--- lib/generators/fwmenu/install/templates/activeadmin/menu_item.rb
+++ lib/generators/fwmenu/install/templates/activeadmin/menu_item.rb
@@ -1,5 +1,17 @@
 ActiveAdmin.register <%= file_name.camelize %>Item do
-	permit_params :title, :link, :show, :page, :level, :internal_link, :<%= file_name %>_id
+	permit_params :title, :link, :show, :page, :level, :internal_link, :<%= file_name %>_id, , :<%= file_name %>_item_id, :article_id
+
+  index do
+    actions
+    column :title
+    column :link
+    column :page
+    column :show
+    column :level
+    column :internal_link
+    column :<%= file_name %>_item
+    column :<%= file_name %>
+  end
 
 	form do |f|
     f.inputs 'Create News' do
@@ -8,6 +20,7 @@ ActiveAdmin.register <%= file_name.camelize %>Item do
       f.input :link
       f.input :page, as: :select, collection: Rails.application.routes.routes.collect {|r| r.path.spec.to_s }.compact.uniq.delete_if{|i|i.include? "admin" or i.include? "rails" }.sort
       f.input :show
+      f.input :article, label: "Articles (Only for article page)"
       f.input :internal_link
       f.input :<%= file_name %>_item, label: "Parent"
     end
