3d718c9aa0de624f925e51e31eaabbcff2a9d24b
Brian <brian@futureworkz.com>
2015-10-29 09:03:33 +0700

    update menu_item model

--- lib/generators/fwmenu/install/templates/activeadmin/model/menu_item.rb
+++ lib/generators/fwmenu/install/templates/activeadmin/model/menu_item.rb
@@ -5,5 +5,13 @@ class <%= file_name.camelize %>Item < ActiveRecord::Base
 
 	belongs_to :<%= file_name %>
 	belongs_to :<%= file_name %>_item
+	has_many 	 :<%= file_name %>_items
+	belongs_to :article
+
+	before_save :set_level
+
+	def set_level
+		self.level = self.<%= file_name %>_item.level + 1 if self.<%= file_name %>_item.present?
+	end
 
 end
--- lib/generators/fwmenu/install/templates/menu_item.rb
+++ lib/generators/fwmenu/install/templates/menu_item.rb
@@ -5,8 +5,14 @@ class <%= file_name.camelize %>Item < ActiveRecord::Base
 
 	belongs_to :<%= file_name %>
 	belongs_to :<%= file_name %>_item
+	has_many 	 :<%= file_name %>_items
+	belongs_to :article
 
 	enumerize :page, in: Rails.application.routes.routes.collect {|r| r.path.spec.to_s }.compact.uniq.delete_if{|i|i.include? "admin" or i.include? "rails" }.sort
 	# enumerize :page, in: Rails.app lication.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.delete_if{|i|i.include? "admin"}.sort
+	before_save :set_level
 
+	def set_level
+		self.level = self.<%= file_name %>_item.level + 1 if self.<%= file_name %>_item.present?
+	end
 end
