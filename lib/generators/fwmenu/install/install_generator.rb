e6cb63a7a3a674d308b91edc3b2bd9e4919d8d1a
Brian <brian@futureworkz.com>
2015-11-02 11:17:13 +0700

    update install_generator

--- lib/generators/fwmenu/install/install_generator.rb
+++ lib/generators/fwmenu/install/install_generator.rb
@@ -21,8 +21,10 @@ module Fwmenu
         migration_template "migration/create_menus.rb", "db/migrate/create_#{file_name}s.rb"
         migration_template "migration/create_menu_items.rb", "db/migrate/create_#{file_name}_items.rb"
         migration_template "migration/create_articles.rb", "db/migrate/create_articles.rb"
+        migration_template "migration/create_categories.rb", "db/migrate/create_categories.rb"
         migration_template "migration/add_reference_menu_items_to_menu_item.rb", "db/migrate/add_reference_#{file_name}_items_to_#{file_name}_item.rb"
         migration_template "migration/add_reference_menu_items_to_article.rb", "db/migrate/add_reference_#{file_name}_items_to_article.rb"
+        migration_template "migration/add_reference_articles_to_category.rb", "db/migrate/add_reference_articles_to_category.rb"
       end
 
       def copy_initializer_file
@@ -30,19 +32,26 @@ module Fwmenu
 
         template "models/menu.rb", "app/models/#{file_name}.rb"
         template "models/article.rb", "app/models/article.rb"
+        template "models/category.rb", "app/models/category.rb"
         template "helpers/menu_helper.rb", "app/helpers/#{file_name}_helper.rb"
         copy_file "controllers/articles_controller.rb", "app/controllers/articles_controller.rb"
+        copy_file "controllers/categories_controller.rb", "app/controllers/categories_controller.rb"
         template "views/_get_menu_for.html.slim", "app/views/_get_#{file_name}_for.html.slim"
+        template "views/_menu.html.slim", "app/views/_#{file_name}.html.slim"
         template "views/_get_submenu_for.html.slim", "app/views/_get_sub#{file_name}_for.html.slim"
         copy_file "views/articles/show.html.slim", "app/views/articles/show.html.slim"
         copy_file "views/articles/show/article.html.slim", "app/views/articles/show/article.html.slim"
         copy_file "views/articles/show/latest.html.slim", "app/views/articles/show/latest.html.slim"
+        copy_file "views/categories/show.html.slim", "app/views/categories/show.html.slim"
+        copy_file "views/categories/show/article.html.slim", "app/views/categories/show/article.html.slim"
+        copy_file "views/categories/show/latest.html.slim", "app/views/categories/show/latest.html.slim"
 
         if active.include? "activeadmin"
           template "activeadmin/model/menu_item.rb", "app/models/#{file_name}_item.rb"
           template "activeadmin/menu.rb", "app/admin/#{file_name}.rb"
           template "activeadmin/menu_item.rb", "app/admin/#{file_name}_item.rb"
           template "activeadmin/article.rb", "app/admin/article.rb"
+          template "activeadmin/category.rb", "app/admin/category.rb"
         else
           template "models/menu_item.rb", "app/models/#{file_name}_item.rb"
         end
