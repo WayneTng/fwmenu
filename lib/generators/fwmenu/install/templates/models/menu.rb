e880f4dcd4b89237222dfb4195741f223454b7b6
Brian <brian@futureworkz.com>
2015-11-02 14:37:37 +0700

    update menu model

--- lib/generators/fwmenu/install/templates/models/menu.rb
+++ lib/generators/fwmenu/install/templates/models/menu.rb
@@ -1,5 +1,8 @@
 class <%= class_name %> < ActiveRecord::Base
+	extend Enumerize
 	validates :title, :position, presence: true
+
+	enumerize :position, in: Position.all.map(&:title)
 
 	has_many :<%= file_name %>_items
 end
