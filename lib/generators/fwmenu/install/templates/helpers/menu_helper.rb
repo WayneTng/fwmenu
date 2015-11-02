86a1a63c7daeb92747225c86072d8697fab9a6ea
Brian <brian@futureworkz.com>
2015-11-02 11:13:35 +0700

    update menu_helper

--- lib/generators/fwmenu/install/templates/helpers/menu_helper.rb
+++ lib/generators/fwmenu/install/templates/helpers/menu_helper.rb
@@ -6,7 +6,7 @@ module <%= class_name %>Helper
   def get_link(item)
 		if item.internal_link && item.page.present?
 			if item.page.to_s == "/articles/:id" && item.article.present?
-				link_to item.title, article_url(item.article)
+				link_to item.title, Rails.application.routes.url_helpers.article_path(item.article)
 			elsif item.page.include? ":id"
 				link = item.page.gsub(":id", item.show.to_s)
 				link_to item.title, link
