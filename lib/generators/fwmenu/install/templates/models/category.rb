2edc5313d12875b84eb8d20bb6efd0b37db2eae8
Brian <brian@futureworkz.com>
2015-11-02 11:40:21 +0700

    update category model

--- lib/generators/fwmenu/install/templates/models/category.rb
+++ lib/generators/fwmenu/install/templates/models/category.rb
@@ -1,7 +1,15 @@
 class Category < ActiveRecord::Base
+	extend Enumerize
+	extend FriendlyId
 	has_many :articles
+
+	friendly_id :title, use: [:slugged, :finders]
 
 	validates :title, :description, presence: true
 
 	enumerize :layout, in: Dir.glob('app/views/categories/show/*').collect{|r| r.gsub("app/views/", "").split('.').first}
+
+	def set_slug
+  	self.slug = title.parameterize unless slug.present?
+  end
 end
