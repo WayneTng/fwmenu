bedc484e65ece7a357d740b0c2a7fde959e96392
Brian <brian@futureworkz.com>
2015-11-02 11:16:26 +0700

    update menu_item model

--- lib/generators/fwmenu/install/templates/models/menu_item.rb
+++ lib/generators/fwmenu/install/templates/models/menu_item.rb
@@ -12,6 +12,28 @@ class <%= file_name.camelize %>Item < ActiveRecord::Base
 	
 	before_save :set_level
 	before_validation :validation_custom
+	after_save :save_<%= file_name %>_to_module
+
+	def save_<%= file_name %>_to_module
+		items = {"items" => [<%= file_name %>]}
+		html = render_anywhere("<%= file_name %>", items)
+		mod_<%= file_name %>s = Place.where(position: <%= file_name %>.position)
+
+		if mod_<%= file_name %>s.size == 1
+			mod_<%= file_name %>s.first.update(description: html)
+		else		
+			mod_<%= file_name %> = Place.new(title: title, description: html, page: "All", position: <%= file_name %>.position)
+			mod_<%= file_name %>.save
+		end
+		true
+	end
+
+	def render_anywhere(partial, assigns = {}) 
+		view = ActionView::Base.new(ActionController::Base.view_paths, assigns)
+		view.extend ApplicationHelper
+		view.extend <%= file_name.camelize %>Helper
+		view.render(:partial => partial)
+	end
 
 	def set_level
 		self.level = self.<%= file_name %>_item.level + 1 if self.<%= file_name %>_item.present?
@@ -57,5 +79,6 @@ class <%= file_name.camelize %>Item < ActiveRecord::Base
 
 		errors.add(:<%= file_name %>_item, "Please select parent belong to <%= file_name %> group") if <%= file_name %>_item.present? && <%= file_name %>.id != <%= file_name %>_item.<%= file_name %>.id
 		errors.add(:<%= file_name %>_item, "Please don't select parent is sub<%= file_name %> of this item or itself") if <%= file_name %>_item.present? && (get_child(self, arr).map(&:id).include?(<%= file_name %>_item_id) || id == <%= file_name %>_item_id)
+		errors.add(:<%= file_name %>, "Please can not change <%= file_name %> group.") if <%= file_name %>_id.present? && <%= file_name %>_was.present? && <%= file_name %>_id != <%= file_name %>_id_was
 	end
 end
