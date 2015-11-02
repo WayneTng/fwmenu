0b8c684fa5851c2915e44721628d9a17c952a3df
Brian <brian@futureworkz.com>
2015-11-02 13:07:05 +0700

    update article to active admin

--- lib/generators/fwmenu/install/templates/activeadmin/article.rb
+++ lib/generators/fwmenu/install/templates/activeadmin/article.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Article do
-	permit_params :title, :description, :slug, :layout
+	permit_params :title, :description, :slug, :layout, :category_id
+
+	index do
+    actions
+    column :title
+    column :slug
+    column :layout
+    column :category
+    column :created_at
+    column :updated_at
+  end
 end
