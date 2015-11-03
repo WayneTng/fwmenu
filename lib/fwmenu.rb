b12ac60af3f7a8b155cdebc8e1de73f1b44dade2
Brian <brian@futureworkz.com>
2015-11-03 15:31:53 +0700

    update fwmenu.rb

--- lib/fwmenu.rb
+++ lib/fwmenu.rb
@@ -4,6 +4,6 @@ require "friendly_id"
 require "slim-rails"
 
 module Fwmenu
-  ActionView::Base.send :include, MenuHelper
-  ActionView::Base.send :include, PlaceHelper
+  class Engine < ::Rails::Engine
+  end
 end
