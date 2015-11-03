6b96c782943f4c810754ddbb89a86ad9d1a13086
Brian <brian@futureworkz.com>
2015-11-03 14:17:03 +0700

    remove change_table

--- lib/generators/fwcontent/install/templates/content_helper.rb
+++
@@ -1,5 +0,0 @@
-module <%= class_name %>Helper
-  def get_content_for
-    <%= class_name %>.where("page like ? or page like ?", "%#{params[:controller]}:#{params[:action]}\n%", "%All%")
-  end
-end
