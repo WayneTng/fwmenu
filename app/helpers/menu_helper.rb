e1a278da03d71c0a3bd9aa4f59b3926c2cfe2149
Brian <brian@futureworkz.com>
2015-11-04 13:12:11 +0700

    update menu_helper

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -6,17 +6,17 @@ module MenuHelper
   def get_link(item)
 		if item.internal_link && item.page.present?
 			if item.page.to_s == "/articles/:id" && item.article.present?
-				link_to item.title, Rails.application.routes.url_helpers.article_path(item.article)
+				link_to item.title, Rails.application.routes.url_helpers.article_path(item.article), target: item.target
 			elsif item.page.to_s == "/categories/:id" && item.category.present?
-				link_to item.title, Rails.application.routes.url_helpers.category_path(item.category)
+				link_to item.title, Rails.application.routes.url_helpers.category_path(item.category), target: item.target
 			elsif item.page.include? ":id"
 				link = item.page.gsub(":id", item.show.to_s)
-				link_to item.title, link
+				link_to item.title, link, target: item.target
 			else
-				link_to item.title, item.page
+				link_to item.title, item.page, target: item.target
 			end
 		else
-			link_to item.title, item.link, target: :_blank
+			link_to item.title, item.link, target: item.target
 		end
 	end
 end
