9ece17ad34eb2e93099b93466d0c04d4aafa518b
Brian <brian@futureworkz.com>
2015-11-04 10:36:12 +0700

    add spec to gem

---
+++ spec/lib/fwmenu_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'fwmenu'
+require_relative '../../app/helpers/menu_helper.rb'
+require_relative '../../app/helpers/place_helper.rb'
+
+describe MenuHelper do
+  it 'should include MenuHelper' do
+   	should be MenuHelper
+  end
+end
+
+describe PlaceHelper do
+  it 'should include PlaceHelper' do
+   	should be PlaceHelper
+  end
+end
