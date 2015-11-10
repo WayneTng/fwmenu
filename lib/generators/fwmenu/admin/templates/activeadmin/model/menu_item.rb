ba15d894e2d3dbc32a5e0480ce6a7772268c73e5
Brian <brian@futureworkz.com>
2015-11-10 17:31:31 +0700

    update menu_item model

--- lib/generators/fwmenu/admin/templates/activeadmin/model/menu_item.rb
+++ lib/generators/fwmenu/admin/templates/activeadmin/model/menu_item.rb
@@ -20,14 +20,14 @@ class <%= file_name.camelize %>Item < ActiveRecord::Base
 	end
 
 	def save_<%= file_name %>_to_module(single_menu)
-		items = {"items" => [single_<%= file_name %>]}
+		items = {"items" => [single_menu]}
 		html = render_anywhere("<%= file_name %>", items)
 		html = "<ul></ul>" if html.empty?
-		mod_<%= file_name %>s = Place.where(position: single_<%= file_name %>.position)
+		mod_<%= file_name %>s = Place.where(position: single_menu.position)
 		if mod_<%= file_name %>s.size == 1
 			mod_<%= file_name %>s.first.update(description: html)
 		else		
-			mod_<%= file_name %> = Place.new(title: title, description: html, page: "All", position: single_<%= file_name %>.position)
+			mod_<%= file_name %> = Place.new(title: single_menu.title, description: html, page: "All", position: single_menu.position)
 			mod_<%= file_name %>.save
 		end
 	end
