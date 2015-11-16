7fccccb3a0aaa98ae00b25a17c3ca8ec8ad42276
Brian <brian@futureworkz.com>
2015-11-16 11:34:10 +0700

    update content model

--- lib/generators/fwcontent/admin/templates/rails_admin/model/content.rb
+++ lib/generators/fwcontent/admin/templates/rails_admin/model/content.rb
@@ -6,7 +6,7 @@ class <%= class_name %> < ActiveRecord::Base
 	serialize :page
 
 	def page_enum
-    Rails.application.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.sort
+    (Rails.application.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" } << "All").compact.uniq.delete_if{ |i| i.include? "admin" or i.include? "rails" }.sort
   end
 
   rails_admin do
